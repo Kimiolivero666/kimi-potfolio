df3cf1f6d7bc39358ad80e413c1ea211cc5a0169
kimi-olivero <migueloliveropazos@gmail.com>
2024-02-21 20:22:40 +0000

    terminacion empresas

--- src/component/empresas/Empresas.jsx
+++ src/component/empresas/Empresas.jsx
@@ -6,20 +6,25 @@ import fiverr from '../../assets/fiverr.png'
 import upwork from '../../assets/upwork.png'
 import freelancer from '../../assets/freelancer.png'
 import malt from '../../assets/malt.png'
+import { Col, Row } from 'react-bootstrap'
 
 const Empresas = () => {
   return (
     <div className='Empresas'>
-        <div className="empresas-titulo text-center">
-        <Titulo titulo='Empresas'/>
+      <div className="empresas-titulo text-center">
+        <Titulo titulo='Empresas' />
+      </div>
+      <div className="logos-empresas text-center">
+         <Row >
+          <Col  >
+          <img src={fiverr} alt="" />
+          <img src={upwork} alt="" />
+          <img src={freelancer} alt="" />
+          <img src={malt} alt="" />
+          </Col>
+        </Row> 
         </div>
-        <div className="logos-empresas">
-            <img src={fiverr} alt="" />
-            <img src={upwork} alt="" />
-            <img src={freelancer} alt="" />
-            <img src={malt} alt="" />
-        </div>
-        
+
     </div>
   )
 }
--- src/paginas/testimonios/Testimonios.jsx
+++ src/paginas/testimonios/Testimonios.jsx
@@ -30,49 +30,53 @@ const Testimonios = () => {
 
             <div className="contenedor-tarjetas">
                 <Row>
-                    <Col lg={2}></Col>
-                    <Col xs={12} lg={8} className='mb-3'>
+                    {/* <Col lg={2}></Col> */}
+                    <Col xs={12}  lg={6} className='mb-3'>
                         <motion.div
                             initial={{ opacity: 0, y: 50 }}
                             animate={isVisible ? { opacity: 1, y: 0 } : {}}
                             transition={{ duration: 0.5, delay: 0.5 }}
+                            className="testimonio-container"
                         >
                             <TestimoniosCard testimonio='Muy profesional y serio. Todo un placer trabajar con el. Volveremos a trabajar con el en el futuro' imgTestimonio="https://img.freepik.com/foto-gratis/retrato-empleado-feliz-volver-al-trabajo_23-2148727616.jpg?w=1060&t=st=1708024332~exp=1708024932~hmac=474d8225a13b46725abdb2b05b361e0db52624e3e565a60b8ba146af254cbad9" nombre='Borja' cliente="INTERFUNERARIAS" />
                         </motion.div>
                     </Col>
-                </Row>
-                <Row>
+                {/* </Row> */}
+                {/* <Row> */}
 
-                    <Col lg={2}></Col>
-                    <Col xs={12} lg={8} className='mb-3'>
+                    {/* <Col lg={2}></Col> */}
+                    <Col xs={12}  lg={6} className='mb-3'>
                         <motion.div
                             initial={{ opacity: 0, y: 50 }}
                             animate={isVisible ? { opacity: 1, y: 0 } : {}}
                             transition={{ duration: 0.5, delay: 1 }}
+                            className="testimonio-container"
                         >
                             <TestimoniosCard testimonio='La experiencia al trabajar con Miguel ha sido muy buena. Realizó el trabajo solicitado correctamente y hubo buena comunicación. También estuvo dispuesto a hacer los cambios que le pedimos y se esforzó por cumplir con los plazos. Le tendremos en cuenta para más proyectos en el futuro.' imgTestimonio={Waola} nombre='Woala' cliente="WOA" />
                         </motion.div>
                     </Col>
-                </Row>
-                <Row>
-                <Col lg={2}></Col>
-                <Col xs={12} lg={8} className='mb-3'>
+                {/* </Row> */}
+                {/* <Row> */}
+                {/* <Col lg={2}></Col> */}
+                <Col xs={12}  lg={6} className='mb-3'>
                     <motion.div
                         initial={{ opacity: 0, y: 50 }}
                         animate={isVisible ? { opacity: 1, y: 0 } : {}}
                         transition={{ duration: 0.5, delay: 1.5 }}
+                        className="testimonio-container"
                     >
-                        <TestimoniosCard testimonio='Kimi (Miguel) es un gran profesional, estamos muy contentos con el trabajo realizado y el trato, seguiremos contando con él para próximos proyectos. Lo recomiendo sin ninguna duda.' imgTestimonio={Alexander} nombre='Alexandre' cliente="EVENTS ARENA" />
+                        <TestimoniosCard testimonio='Kimi (Miguel) es un gran profesional, estamos muy contentos con el trabajo realizado y el trato, seguiremos contando con él para próximos proyectos. Lo recomiendo sin ninguna duda.    ' imgTestimonio={Alexander} nombre='Alexandre' cliente="EVENTS ARENA" />
                     </motion.div>
                 </Col>
-                </Row>
-                <Row>
-                <Col lg={2}></Col>
-                <Col xs={12} lg={8} className='mb-3'>
+                {/* </Row> */}
+                {/* <Row> */}
+                {/* <Col lg={2}></Col> */}
+                <Col xs={12}  lg={6} className='mb-3'>
                     <motion.div
                         initial={{ opacity: 0, y: 50 }}
                         animate={isVisible ? { opacity: 1, y: 0 } : {}}
                         transition={{ duration: 0.5, delay: 2 }}
+                        className="testimonio-container"
                     >
                         <TestimoniosCard testimonio='Rigor, seriedad, rapidez y profesionalidad. Inmejorable.' imgTestimonio={Miguel} nombre='Miguel' cliente="GRUPO MAINJOBS" />
                     </motion.div>
