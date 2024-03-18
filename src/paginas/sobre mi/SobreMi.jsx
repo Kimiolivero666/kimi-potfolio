efcdf25d113877c61c58cc06b4328a855c922ef3
kimi-olivero <migueloliveropazos@gmail.com>
2024-03-18 16:18:56 +0000

    arreglo img y parrafo  inicio

--- src/paginas/sobre mi/SobreMi.jsx
+++ src/paginas/sobre mi/SobreMi.jsx
@@ -2,7 +2,7 @@
 import {  Container } from 'react-bootstrap'
 import './sobreMi.css'
 import Titulo from '../../component/titulo/Titulo'
-import laptop from '../../assets/Laptop.png'
+
 import Carrusel from '../../component/carrusel/Carrusel'
 
 const SobreMi = () => {
@@ -15,14 +15,13 @@ const SobreMi = () => {
                 <Container>
                     
                         <div className='sobre-mi-imagen'>
-                            <img src={laptop} alt="" />
                         </div>
                         <div className='sobre-mi-info '>
 
-                            <p>¡Hola! Soy Miguel Olivero, aunque me conocen como KIMI, un apasionado desarrollador front-end con un enfoque creativo y meticuloso en la creación de experiencias web cautivadoras.</p>
-                            <p>Mi fascinación por la tecnología y el diseño me impulsa a mantenerme actualizado con las últimas tendencias y mejores prácticas en desarrollo web.</p>
-                            <p>Además de mi habilidad técnica, me considero un comunicador efectivo y un solucionador de problemas creativo. Disfruto colaborar en equipos multifuncionales para crear soluciones innovadoras que superen las expectativas del cliente.</p>
-                            <p>Cuando no estoy codificando, me puedes encontrar explorando nuevas herramientas y frameworks, o disfrutando de actividades al aire libre. Estoy emocionado por seguir creciendo profesionalmente y contribuir al emocionante mundo del desarrollo web.</p>
+                            <p>¡Hola! Soy Miguel Olivero, aunque me conocen como KIMI, un apasionado desarrollador front-end con un enfoque creativo y meticuloso en la creación de experiencias web cautivadoras. Mi fascinación por la tecnología y el diseño me impulsa a mantenerme actualizado con las últimas tendencias y mejores prácticas en desarrollo web.</p>
+                        
+                            <p>Además de mi habilidad técnica, me considero un comunicador efectivo y un solucionador de problemas creativo. Disfruto colaborar en equipos multifuncionales para crear soluciones innovadoras que superen las expectativas del cliente. Cuando no estoy codificando, me puedes encontrar explorando nuevas herramientas y frameworks, o disfrutando de actividades al aire libre. Estoy emocionado por seguir creciendo profesionalmente y contribuir al emocionante mundo del desarrollo web.</p>
+                            
                         </div>
 
                         <Carrusel/>
